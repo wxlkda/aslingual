a08f242f456bb53f997d68500c174fa80fd39eb5
Alwalid Khan <71565457+wxlkda@users.noreply.github.com>
2023-10-30 18:09:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import cv2
 import time
 import mediapipe as mp
+import numpy as np
+
+class HandGestureModel:
+    def __init__(self):
+        pass
+
+    def predict(self, landmarks):
+        thumb_is_open = landmarks[mp.solutions.hands.HandLandmark.THUMB_TIP].y < landmarks[mp.solutions.hands.HandLandmark.THUMB_MCP].y
+        index_is_open = landmarks[mp.solutions.hands.HandLandmark.INDEX_FINGER_TIP].y < landmarks[mp.solutions.hands.HandLandmark.INDEX_FINGER_MCP].y
+        middle_is_open = landmarks[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_TIP].y < landmarks[mp.solutions.hands.HandLandmark.MIDDLE_FINGER_MCP].y
+        ring_is_open = landmarks[mp.solutions.hands.HandLandmark.RING_FINGER_TIP].y < landmarks[mp.solutions.hands.HandLandmark.RING_FINGER_MCP].y
+        pinky_is_open = landmarks[mp.solutions.hands.HandLandmark.PINKY_TIP].y < landmarks[mp.solutions.hands.HandLandmark.PINKY_MCP].y
+        
+        fingers_open = np.array([thumb_is_open, index_is_open, middle_is_open, ring_is_open, pinky_is_open])
+        num_fingers_open = np.sum(fingers_open)
+
+        if num_fingers_open == 5:
+            return "Open Hand"
+        elif num_fingers_open == 0:
+            return "Closed Fist"
+        else:
+            return "Unknown Gesture"
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.8)  # no more than 2 hands
+hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.8)
 mp_drawing = mp.solutions.drawing_utils
 
 WIDTH = 1366
@@ -23,6 +45,8 @@ start_time = time.time()
 left_hand_detected = False
 right_hand_detected = False
 
+gesture_model = HandGestureModel()
+
 while video_capture.isOpened():
     ret, frame = video_capture.read()
     frame = cv2.flip(frame, 1)
@@ -30,12 +54,13 @@ while video_capture.isOpened():
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(frame_rgb)
     
-
     left_hand_detected = False
     right_hand_detected = False
     
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
+            gesture = gesture_model.predict(hand_landmarks.landmark)
+            cv2.putText(frame, gesture, (10, 60), FONT, FONT_SCALE, (255, 255, 255), THICKNESS)
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
             if hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x < 0.5:
                 left_hand_detected = True
